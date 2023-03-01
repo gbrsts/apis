3201a7cc7abb1e183bd63e45975b398756de1ca6
gbr <gbr_sts@yahoo.com>
2023-03-01 16:01:53 -0300

    terminate: nodejs + express api

--- api-1-node-express/app.js
+++ api-1-node-express/app.js
@@ -2,25 +2,86 @@ const express = require("express");
 
 const { randomUUID } = require("crypto")
 
+const fs = require("fs")
+
 const app = express();
 
 app.use(express.json());
 
-const products = [];
+let products = [];
+
+fs.readFile("products.json", "utf-8", (error, data) =>{
+	if (error) {
+		console.log(error)
+	} else {
+		products = JSON.parse(data)
+	}
+})
+
+function newFile() {
+	fs.writeFile("products.json", JSON.stringify(products), (error) => {
+		if (error) {
+			console.log(error)
+		} else {
+			console.log("Produto cadastrado")
+		}
+	})
+}
 
 app.post("/products", (request, response) => {
-    
-    const { name, price } = request.body;
+	
+	const { name, price } = request.body;
 
-    const product = {
-        name,
-        price,
-        id: randomUUID()
-    }
+	const product = {
+		name,
+		price,
+		id: randomUUID()
+	};
 
-    products.push(product)
+	products.push(product);
 
-    return response.json(product)
+	newFile()
+
+	return response.json(product);
 })
+
+app.get("/products", (request, response) => {
+	return response.json(products);
+})
+
+app.get("/products/:id", (request, response) => {
+	const { id } = request.params;
+	const product = products.find((product) => product.id === id);
+	return response.json(product);
+})
+
+app.put("/products/:id", (request, response) => {
+	const { id } = request.params;
+	const { name, price } = request.body;
+
+	const productIndex = products.findIndex((product) => product.id === id);
+	products[productIndex] = {
+		...products[productIndex],
+		name,
+		price
+	};
+
+	newFile()
+
+	return response.json({ message: "Produto alterado com sucesso" });
+})
+
+app.delete("/products/:id", (request, response) => {
+	const { id } = request.params;
+
+	const productIndex = products.findIndex((product) => product.id === id);
+
+	products.splice(productIndex, 1)
+
+	newFile()
+
+	return response.json({ message: "Produto excluido com sucesso" })
+})
+
 
 app.listen(4002, () => console.log("Server on port 4002"));
--- api-1-node-express/server.js
+++ api-1-node-express/server.js
@@ -2,22 +2,22 @@ const http = require("http");
 
 http.createServer((request, response) => {
 
-    response.writeHead(200, {"Content-Type": "application/json"});
-    
-    if (request.url === "/product") {
-        response.end(
-            JSON.stringify({
-                mensage: "Product route"
-            })
-        )
-    }
-    
-    if (request.url === "/user") {
-        response.end(
-            JSON.stringify({
-                mensage: "User route"
-            })
-        )
-    }
+	response.writeHead(200, {"Content-Type": "application/json"});
+	
+	if (request.url === "/product") {
+		response.end(
+			JSON.stringify({
+				mensage: "Product route"
+			})
+		)
+	}
+	
+	if (request.url === "/user") {
+		response.end(
+			JSON.stringify({
+				mensage: "User route"
+			})
+		)
+	}
 
 }).listen(4001, () => console.log("Server on port 4001"))
